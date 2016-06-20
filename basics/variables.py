b36dbb93e1fd86e8daa4216f18aa9af4e359acd5
Ke Wang <kw429@cornell.edu>
2016-06-20 18:30:31 -0500

    variables

---
+++ basics/variables.py
@@ -0,0 +1,35 @@
+# 1. get type of variable
+a = 7
+print(type(a))
+
+# 2. math operators, not in python 3 I don't need to worry about overflow
+# + addition
+# - subtraction
+# * multiplication
+# / floating point division
+# // integer (truncating) division % modulus (remainder)
+# ** exponentiation
+
+# 3. base
+# decimal
+print(10)
+# binary
+print(0b10)
+# octal
+print(0o10)
+# hexadecimal
+print(0x10)
+
+# 4. type conversion, same for float
+print(int(True))
+print(int(98.6))
+print(int(1.0e4))
+print(int('+13'))
+
+# 5. None is useful to distinguish a missing value from an empty value. same as 'null'
+nothing = None
+
+# Zen of Python
+import this
+
+print(this)
