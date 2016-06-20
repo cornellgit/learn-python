5b15b7afca2f833c1bdef6f985523365107b66b8
Ke Wang <kw429@cornell.edu>
2016-06-20 16:01:02 -0500

    dict comprehension

--- basics/dict.py
+++ basics/dict.py
@@ -6,6 +6,10 @@ dict['o'] = 'omega'
 # more succinct
 dict = {'a': 'alpha', 'g': 'gamma', 'o': 'omega'}
 
+# dict comprehension, reverse key and value
+dict_rev = {desc: ch for ch, desc in dict.items()}
+print(dict_rev)
+
 # contains key
 if 'a' in dict:
     print('found ' + dict.get('a'))
