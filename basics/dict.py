2a420dd7c0b325a4eeaa95b285434976f2f85181
Ke Wang <kw429@cornell.edu>
2016-06-21 14:51:33 -0500

    handling missing key

--- basics/dict.py
+++ basics/dict.py
@@ -1,4 +1,4 @@
-# empty dict {}
+# 1.1 initialization, empty dict {}
 dict = {}
 dict['a'] = 'alpha'
 dict['g'] = 'gamma'
@@ -6,28 +6,40 @@ dict['o'] = 'omega'
 # more succinct
 dict = {'a': 'alpha', 'g': 'gamma', 'o': 'omega'}
 
-# dict comprehension, reverse key and value
-dict_rev = {desc: ch for ch, desc in dict.items()}
-print(dict_rev)
-
-# contains key
+# 1.2 access missing key
 if 'a' in dict:
     print('found ' + dict.get('a'))
 
-# keys and values, and k-v pairs
+dict.setdefault('not_exist_key', '0')
+
+# 1.3 default dict
+from collections import defaultdict
+food_counter = defaultdict(int)
+for food in ['spam', 'spam', 'eggs', 'spam']:
+    food_counter[food] += 1
+
+print(food_counter)
+
+
+# 2. dict comprehension, reverse key and value
+dict_rev = {desc: ch for ch, desc in dict.items()}
+print(dict_rev)
+
+
+# 3. keys and values, and k-v pairs
 print(dict.keys())
 print(dict.values())
 print(dict.items())
 
-# loop through
+# 4. loop through
 for k, v in dict.items():
     print(k + '->' + v)
 
 # iterkeys(), itervalues() and iteritems() which avoid the cost of constructing the whole list
-# string format
+# 5. string format
 hash = {'word': 'garfield', 'count': 42}
 print('I want %(count)d copies of %(word)s' % hash)
 
-# remove
+# 6. remove
 del hash['word']
 print(hash)
